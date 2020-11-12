0eeb582d70c8f6db160678326d0eee9c844f521f
LinaGkiosi <ligkio@gmail.com>
2020-11-12 19:26:15 +0200

    commit2

--- login2.php
+++ login2.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<?php
+		$con = mysqli_connect("localhost","root","dbgame");
+		if(isset($_POST['log'])){
+			$username = mysqli_real_escape_string($con,$_POST['username']);
+		}
 </head>
 <body>
-	<div id='frm'>
-		<form action="do_login.php" method="POST">
-			<p>
-				<label>Username:</label>
-				<input type="text" id="user" name="user" />
-			</p>
-			<p>
-				<label>Password:</label>
-				<input type="password" id="pass" name="pass" />
-			</p>
-			<p>
-				<input type="submit" id="btn" name="Login">
-		</form>
-	</div>
+		<form class="" action="login2.php" method="POST">
+			<div class="container">
+				<label for="">Username/Password</label>
+				<input type="text" id="username" placeholder="Username" name="username" />
+				<input type="password" id="pass" placeholder="Password"name="password" />
+				<button type="submit" class="btn" name="log">Login</button>
+		</div>
+	</form>
 </body>
 </html>
