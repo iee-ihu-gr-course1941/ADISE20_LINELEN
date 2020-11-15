059e58ba23e3f42e026c8d1da6e23c22d03904aa
LinaGkiosi <ligkio@gmail.com>
2020-11-15 18:13:58 +0200

    Commit4

--- game.php
+++ game.php
@@ -1,4 +1,39 @@
-<?php
-	echo "Hello world!<br>";
-	
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Game</title>
+	 <link rel="stylesheet" type="text/css" href="css/game.css">
+</head>
+<body>
+<div id="board">
+        <div id="Red">
+            <img src="./image/red.png" alt="Red">
+        </div>
+        <div id="top_card">
+            <img src="./image/display.png" alt="top_card">
+        </div>
+
+        <div class="player_box top">
+            <p class="name">Player 1</p>
+            <p class="number_of_cards">0</p>
+        </div>
+        <div class="player_box left">
+            <p class="name">Player 2</p>
+            <p class="number_of_cards">0</p>
+        </div>
+        <div class="player_box right">
+            <p class="name">Player 4</p>
+            <p class="number_of_cards">0</p>
+        </div>
+        <div class="player_box bottom">
+            <p class="name">Player 3</p>
+            <p class="number_of_cards">0</p>
+        </div>
+
+        <button id="pass">Pass</button>
+        <button id="assign_color">Assign Color</button>
+</div>
+        <div id="hand">
+        </div>
+</body>
+</html>
