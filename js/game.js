2fc165f4bcea98752885dc50499c6e9aabb41458
elenialexiou <eleni.a242@gmail.com>
2020-12-15 16:37:15 +0200

    commit8

--- js/game.js
+++ js/game.js
@@ -1,13 +1,3 @@
-function unhide() {
-                    var hid = document.getElementsByClassName("exp");
-                    // Emulates jQuery $(element).is(':hidden');
-                    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
-                        hid[0].style.visibility = "visible";
-                    }
-                }
-
-
-
 function show_opponent() {
 	alert(
 					"ololololo"
