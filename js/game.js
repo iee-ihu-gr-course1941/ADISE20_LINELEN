711e4b1fdf8ee6e15b53e8915ccb9adffa58f5cb
LinaGkiosi <ligkio@gmail.com>
2020-12-22 16:35:28 +0200

    CommitL

--- js/game.js
+++ js/game.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 	show_opponent();
-}
+});
+
+
 
 function show_opponent() {
-
-	document.getElementById("opponent").innerHTML == "Waiting for opponent...";
-		
+if (
+		document.getElementById("opponent").innerHTML == "Waiting for opponent..."
+	) {
+		setInterval(function() {
+			$("#opponent")
+				.load("show_opponent.php")
+				.fadeIn("slow");
+		}, 3000);
+	}
 }
 
 
