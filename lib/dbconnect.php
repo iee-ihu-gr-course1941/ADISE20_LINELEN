4078e80335a00f83bcb1dfd3fc95bae7f8c1dfd5
elenialexiou <eleni.a242@gmail.com>
2021-01-02 21:04:00 +0200

    commit 13

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,17 +1,19 @@
 <?php
-
 $host='localhost';
 $db = 'project_db';
-
 require_once "config_local.php";
 
 $user=$DB_USER;
 $pass=$DB_PASS;
 
-global $mysqli;
-$mysqli = new mysqli($host, $user, $pass, $db);    
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2016/it164631/mysql/run/mysql.sock');
+} else {
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }?>
-
